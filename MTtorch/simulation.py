8e22fefe17836ddba4a0ef1d9cb346bbb20bfa5b
unknown <max.schelski@googlemail.com>
2023-04-20 15:36:47 +0200

    Finished classes and functions; not debugged

--- MTtorch/simulation.py
+++ MTtorch/simulation.py
@@ -31,7 +31,7 @@ def nucleation(time_array, min_nucleation_fraction=0.1):
     Get nucleation rate at defined time
     """
     # 1 has to be added to sin function to prevent negative nucleation
-    # then normalize nucleation to be within 0 and 1
+    # then normalize nucleation to be within min_nucleation_fraction and 1
     return ((torch.sin(time_array) + 1) / (2 / (1 - min_nucleation_fraction))
             + min_nucleation_fraction)
 
@@ -73,6 +73,10 @@ class simulation():
         self.properties = properties
         self.actions = actions
 
+        self.action_funcs = {}
+        self.action_funcs["add"] = self._add_to_property
+        self.action_funcs["remove"] = self._remove_from_property
+
     def start(self, nb_simulations, min_time, max_number_objects):
         """
 
@@ -113,7 +117,7 @@ class simulation():
             array = model_parameters.values.expand_dims(expand_dimensions)
             # save array in object, therefore also change objects saved in
             # self.transitions and self.actions
-            model_parameters.array = array
+            model_parameters.value_array = array
             # assign model parameter to the correct dimension
             # in the simulation arrays
             self.dimension_to_parameter_map[dimension] = model_parameters
@@ -123,6 +127,7 @@ class simulation():
         # so that each object can have a value for each property
         for object_property in self.properties:
             object_property.array = torch.zeros(self._simulation_array_size)
+            object_property.array = float("nan")
 
         self.times = torch.zeros(self._simulation_array_size)[0, :]
 
@@ -130,13 +135,31 @@ class simulation():
         # in the same state
         self.object_states = torch.zeros(self._simulation_array_size)
 
+        for action in self.actions:
+            array = action.values.expand(self._simulation_array_size)
+            action.value_array = array
+
         while torch.min(self.times) < min_time:
             self._run_iteration()
 
     def _run_iteration(self):
         # create tensor for x (position in neurite), l (length of microtubule)
         # and time
+        total_rates = self._get_total_and_single_rates_for_state_transitions()
 
+        reaction_times = self._get_times_of_next_transition(total_rates)
+
+        self._determine_next_transition(total_rates)
+
+        self._determine_positions_of_transitions()
+
+        self._execute_actions_on_objects(reaction_times)
+
+        self._update_object_states()
+
+        self.times += reaction_times[0]
+
+    def _get_total_and_single_rates_for_state_transitions(self):
         # get number of objects in each state
         nb_objects_all_states = torch.HalfTensor()
         # add 1 to number of states since 0 is not explicitly defined
@@ -163,31 +186,34 @@ class simulation():
         # add current nucleation rate to catastrophe rate for each simulation
         total_rates = torch.sum(all_transition_rates, dim=0)
 
-        # get mask of simulations with 0 total rate (nothing will happen,
-        # stationary state)
-        zero_rate_mask = total_rates == 0
+        return total_rates
 
+    def _get_times_of_next_transition(self, total_rates):
         # get time of next event for each simulation
         exponential_func = torch.distributions.exponential.Exponential
         reaction_times = exponential_func(total_rates,
                                           validate_args=False).sample()
+        return reaction_times
+
+    def _determine_next_transition(self, total_rates):
         # get which event happened in each simulation
         random_numbers = torch.rand(total_rates.shape)
         # set random number in zero rate positions to >1 to make threshold
         # higher than total rate, thereby preventing any reaction from
         # being executed
-        random_numbers[zero_rate_mask] = 1.1
+        random_numbers[total_rates == 0] = 1.1
         thresholds = total_rates * random_numbers
 
         # go through each transition and check whether it will occur
         current_rate_sum = torch.HalfTensor()
-        transition_masks = []
         for transition in self.transitions:
             current_rate_sum += transition.current_rates
-            transition_mask = ((current_rate_sum - thresholds) > zero_tensor)
+            transition_mask = ((current_rate_sum - thresholds) >
+                               self.zero_tensor)
             transition_mask  = transition_mask.expand(*self.object_states.shape)
-            transition_masks.append(transition_mask)
+            transition.simulations_mask = transition_mask
 
+    def _determine_positions_of_transitions(self):
         # create array of index numbers of same shape than MTs
         max_number_objects = self.object_states.shape[0]
         index_array = torch.linspace(1, max_number_objects+1,
@@ -200,9 +226,8 @@ class simulation():
         # first, create mask of index positions, so that each object for each
         # simulation has a unique identifier (index) within this simulation
         # setting all positions where no catastrophe can take place to 0
-        all_transition_positions = []
         for transition_nb, transition in enumerate(self.transitions):
-            transition_mask = transition_masks[transition_nb]
+            transition_mask = transition.simulation_mask[transition_nb]
             possible_transition_positions = torch.clone(index_array)
             # exclude simulations where the transition did not happen
             possible_transition_positions[~transition_mask] = 0
@@ -215,49 +240,75 @@ class simulation():
                                                 idx_positions) &
                                                (possible_transition_positions >
                                                 0), True, False)
-            all_transition_positions.append(transition_positions)
+            transition.transition_positions = transition_positions
 
+    def _execute_actions_on_objects(self, reaction_times):
         # execute actions on objects depending on state, before changing state
         for action in self.actions:
             action_positions = self.object_states == action.state
             property_array = action.object_property.array
+            if type(action.operation) == str:
+                # choose function of the established functions
+                error_msg = (f"The action operation {action.operation}"
+                             f" is not defined. Please choose one of the "
+                             f"following function names: "
+                             f"{', '.join(list(self.action_funcs.keys))}.")
+                if action.operation not in self.action_funcs:
+                    raise ValueError(error_msg)
+                continue
             transformed_property_array = action.operation(property_array,
-                                                          )
+                                                          reaction_times,
+                                                          action.value_array)
+            action.object_property.array = transformed_property_array
 
+    def _update_object_states(self):
+        # update the simulations according to executed transitions
+        for transition in self.transitions:
+            transition_positions = transition.transition_positions
+            self.object_states[transition_positions] = transition.end_state
+            # if state ended in state 0, set property array at position to NaN
+            if transition.end_state == 0:
+                for object_property in self.properties:
+                    object_property.array[transition_positions] = float("nan")
+                continue
+            # if state started in state 0, add new entry in property array
+            if transition.start_state != 0:
+                continue
+            nb_creations = len(torch.nonzero(transition_positions))
+            for object_property in self.properties:
+                if object_property.start_value is None:
+                    # scale random number from min_value to max_value
+                    min_value = object_property.min_value
+                    max_value = object_property.max_value
+                    property_values = (torch.rand((nb_creations)) *
+                                       (max_value - min_value) + min_value)
+                else:
+                    property_values = object_property.start_value
+                object_property.array[transition_positions] = property_values
 
-        # update the simulations according to event
-        MTs[catastrophes_mask] = 0
-        MTs[nucleations_mask] = 1
+    def _add_to_property(self, object_poperty, reaction_times, action_values):
+        return object_poperty + (reaction_times * action_values)
 
-        # set position of newly nucleated microtubules random across neurite
-        nb_nucleations = len(torch.nonzero(nucleations_mask))
-
-        x[nucleations_mask] = torch.rand((nb_nucleations)) * max_x
-
-        MT_reaction_times = torch.clone(reaction_times.expand(*MTs.shape))
-        MT_reaction_times[~MTs] = 0
-
-        MT_growth = torch.clone(MT_reaction_times)
-        MT_growth *= MT_growth_speed
-        # update length of growing MTs (polymerization) using elapsed time
-        l += MT_growth
-
-        MT_reaction_times *= MTRF_speeds_array.expand(*MTs.shape)
-        # update x position of all MTs (MT-RF) using elapsed time
-        x -= MT_reaction_times
-
-        # update time
-        times += reaction_times[0]
-
+    def _remove_from_property(self, object_poperty, reaction_times, action_values):
+        return object_poperty - (reaction_times * action_values)
 
 class object_property():
 
-    def __init__(self, name):
+    def __init__(self, name, min_value=0, max_value=1, start_value=None):
         """
         Args:
             name (String): Name of property
+            min_value (float): Minimum allowed value
+            max_value (float): Maximum allowed value
+            start_value (float): Value at which new objects will be initialized
         """
         self.name = name
+        self.min_value = min_value
+        self.max_value = max_value
+        self.start_value = start_value
+
+        # initialize variables used in simulation
+        self.value_array = torch.HalfTensor([])
 
 
 class action():
@@ -267,21 +318,24 @@ class action():
 
         Args:
             name (String): Name of action
-            state:
-            object_property:
+            state (state object):
+            object_property (object_property object):
             operation (func): function that takes the property tensor,
-                                a tensor of the values
-                                (same shape as the property tensor) and
-                                the time tensor and then
-                                outputs the transformed property tensor
-            values:
+                a tensor of the values (same shape as the property tensor) and
+                the time tensor and then outputs the transformed property tensor
+                Alternatively use one of the following standard_positions:
+                "add", "subtract",
+            values (1D iterable):
 
         """
         self.name = name
         self.state = state
         self.operation = operation
-        self.values = values
+        self.values = torch.HalfTensor(values)
         self.object_property = object_property
+
+        # define variables which will be used in simulation
+        self.value_array = torch.HalfTensor([])
 
 
 class state():
@@ -322,7 +376,10 @@ class state_transition():
             self.rates = lifetime_to_rates_factor / lifetimes
         else:
             self.rates = rates
-        self.values = rates
+        self.values = torch.HalfTensor(rates)
         self.time_dependency = time_dependency
+
         # initialize variable that will be filled during simulation
-        self.array = torch.HalfTensor()
+        self.value_array = torch.HalfTensor([])
+        self.simulation_mask = torch.HalfTensor([])
+        self.transition_positions = torch.HalfTensor([])
