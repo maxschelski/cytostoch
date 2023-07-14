Fixed bugs, e.g. density calculation

cytotorch/analyzer.py
@@ -98,7 +98,7 @@ class Analyzer():
 
         timestep_changes = self._get_timestep_changes_and_repeats(all_times)
 
-        time_array = self._get_equal_timesteps_array(all_times,
+        time_array, repeated_time_mask = self._get_equal_timesteps_array(all_times,
                                                      timestep_changes,
                                                      type="time")
 
@@ -110,17 +110,18 @@ class Analyzer():
             new_data, _ = self._load_data(file_name_keyword=data_keyword)
             new_data = self._equalize_object_nb(new_data)
             new_data = self._concat_arrays_with_removed_param_vals(new_data)
-            new_data = self._get_equal_timesteps_array(new_data,
-                                                       timestep_changes)
+            new_data,_ = self._get_equal_timesteps_array(new_data,
+                                                       timestep_changes,
+                                                       repeated_time_mask)
+
             # sort data by number of datapoints for each timepoint
             # since for each different number of datapoints, a new dataframe
-            all_data[new_data.shape[1]] = {}
+            if new_data.shape[1] not in all_data:
+                all_data[new_data.shape[1]] = {}
             all_data[new_data.shape[1]][data_keyword] = new_data
 
         all_data = self._add_single_datapoint_data_to_other_sets(all_data)
-
         self._save_all(all_data, time_array, parameters)
-
 
         # get expected number of nonzero elements
         # expected_nb_nonzero_elements = ((self.max_timestep+1) *
@@ -163,7 +164,7 @@ class Analyzer():
             new_iteration_nb = iteration_nb.zfill(len(str(max_nb)))
             new_file_name = file_name.replace(iteration_nb+".pt",
                                          new_iteration_nb+".pt")
-            os.rename(os.path.join(self.data_folder, file_name),
+            os.replace(os.path.join(self.data_folder, file_name),
                       os.path.join(self.data_folder, new_file_name))
         return None
 
@@ -259,12 +260,10 @@ class Analyzer():
         current_iteration_nb = 0
         array_resizer = None
         data_concat = torch.FloatTensor()
-        print(data_concat.device)
         # go through all time arrays while keeping track of the current
         # iteration_nb
         #change their size
         for data in data_list:
-            print(data.device)
             # get the current array resizer
             array_resizer = self.array_resizing_dict.get(current_iteration_nb,
                                                          array_resizer)
@@ -384,6 +383,7 @@ class Analyzer():
         return timestep_changes
 
     def _get_equal_timesteps_array(self, input_array, timestep_changes,
+                                   repeated_time_mask=None,
                                    type=None):
         # get timestep changes to same shape a input array...
         # firt change view though
@@ -391,6 +391,7 @@ class Analyzer():
         input_array = input_array.expand([timestep_changes.shape[0],
                                           input_array.shape[1],
                                           *timestep_changes.shape[2:]])
+
         timestep_changes = timestep_changes.expand(input_array.shape)
 
         # move time axis to last position so that all timepoints of the
@@ -433,11 +434,12 @@ class Analyzer():
             # assert used_timepoints.max() <= (self.max_time +
             #                                  self.time_resolution)
 
-        repeated_time_mask = self._get_repeated_timesteps_mask(data_timesteps)
+        if repeated_time_mask is None:
+            repeated_time_mask = self._get_repeated_timesteps_mask(data_timesteps)
         # Use mask to set values of all data arrays to nan
-        data_timesteps[repeated_time_mask] = float("nan")
+        data_timesteps[repeated_time_mask.expand(data_timesteps.shape)] = float("nan")
 
-        return data_timesteps
+        return data_timesteps, repeated_time_mask
 
     def _get_repeated_timesteps_mask(self, time_array):
         # create array for all timepoints
@@ -507,7 +509,7 @@ class Analyzer():
             column_names.append("time")
             # next add all simulation parameters
             for name, parameter in parameters.items():
-                data_values.append(parameter.expand(data_shape)
+                data_values.append(parameter.cpu().expand(data_shape)
                                    .flatten().unsqueeze(1))
                 column_names.append(name)
 
@@ -519,6 +521,7 @@ class Analyzer():
 
             data = torch.concat(data_values, dim=1)
             dataframe = pd.DataFrame(data=data, columns=column_names)
-            file_name = "_".join(column_names[1:])
+            # file_name = "_".join(column_names[1:])
+            file_name = "data"
             dataframe.to_feather(os.path.join(self.data_folder,
                                               file_name + ".feather"))

cytotorch/simulation.py
@@ -8,6 +8,7 @@ import copy
 import numpy as np
 import pandas as pd
 import time
+import shutil
 import sys
 import gc
 import os
@@ -116,7 +117,7 @@ class SSA():
 
         # create tensors object to reference the correct tensor class
         # depending on the device
-        if (device == "GPU") & (torch.cuda.device_count() > 0):
+        if (device.lower() == "gpu") & (torch.cuda.device_count() > 0):
             for GPU_nb in range(torch.cuda.device_count()):
                 if torch.cuda.memory_reserved(GPU_nb) == 0:
                     break
@@ -125,7 +126,7 @@ class SSA():
             torch.set_default_tensor_type(torch.cuda.FloatTensor)
             torch.set_default_device(self.device)
         else:
-            self.device = "CPU"
+            self.device = "cpu"
             self.tensors = torch
             torch.set_default_tensor_type(torch.FloatTensor)
 
@@ -135,17 +136,23 @@ class SSA():
             state.name += "_" + str(state.number)
 
         for transition in self.transitions:
-            if transition.name == "":
+            if transition.name != "":
                 continue
-            transition.name = ("transition_from" +str(self.start_state.number) +
-                               "_to" + str(self.end_state.number))
+            start_state = transition.start_state
+            if start_state is not None:
+                start_state = start_state.number
+            end_state = transition.end_state
+            if end_state is not None:
+                end_state = end_state.number
+            transition.name = ("trans_" + str(start_state) +
+                               "to" + str(end_state))
 
     def start(self, nb_simulations, min_time, data_extraction,data_folder,
               max_number_objects=None,
               ignore_errors=False, print_update_time_step=1,
                nb_objects_added_per_step=10,
                dynamically_increase_nb_objects=True, save_states=False,
-              use_assertion_checks=True):
+              use_assertion_checks=True, reset_folder=True):
         """
 
         Args:
@@ -167,7 +174,7 @@ class SSA():
                         print_update_time_step,
                         nb_objects_added_per_step,
                         dynamically_increase_nb_objects,save_states,
-                        use_assertion_checks)
+                        use_assertion_checks, reset_folder)
 
     def save(self, time_resolution, max_time):
         analysis = analyzer.Analyzer(simulation=self)
@@ -178,7 +185,7 @@ class SSA():
                max_number_objects=None, ignore_errors=False,
                print_update_time_step=1, nb_objects_added_per_step=10,
                dynamically_increase_nb_objects=True, save_states=False,
-               use_assertion_checks=True):
+               use_assertion_checks=True, reset_folder=True):
         """
 
         Args:
@@ -189,6 +196,10 @@ class SSA():
                 simulated. Determines array size
         Returns:
         """
+        if reset_folder:
+            shutil.rmtree(data_folder)
+            os.mkdir(data_folder)
+
         self.min_time = min_time
         self.data_extraction = data_extraction
         self.data_folder = data_folder
@@ -278,7 +289,9 @@ class SSA():
             if whole_time not in times_tracked:
                 print("\n",iteration_nb, "; Current time: ", whole_time * print_update_time_step)
                 times_tracked.add(whole_time)
+
             self._run_iteration(iteration_nb)
+
             iteration_nb += 1
 
         removed_pos_file = os.path.join(self.data_folder,
@@ -986,9 +999,9 @@ class SSA():
                 in zip(all_iteration_nbs, all_data_cpu,
                        all_times_cpu, all_object_states_cpu):
             """
-            data_dict_cpu = {}
-            for keyword, data in data_dict.items():
-                data_dict_cpu[keyword] = data.cpu()
+        data_dict_cpu = {}
+        for keyword, data in data_dict.items():
+            data_dict_cpu[keyword] = data.cpu()
 
         # save data to hard drive
         for file_name, data_array in data_dict_cpu.items():
@@ -1008,7 +1021,6 @@ class SSA():
         return None
 
     def _save_simulation_parameters(self):
-
         for parameter in self._all_simulation_parameters:
             file_name = "param_"+parameter.name+".pt"
             torch.save(parameter.value_array, os.path.join(self.data_folder,