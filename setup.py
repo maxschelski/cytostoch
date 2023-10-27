b16f984b77f074fbf1cd1d58a2d199dd65d497e7
maxschelski <max.schelski@googlemail.com>
2023-10-27 11:14:03 +0200

    Changed package name

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-	name="cytotorch",
+	name="cytostoch",
 	author="Max Schelski",
 	author_email = "max.schelski@googlemail.com",
 	description = "Stochastic simulations of cytoskeletal structures.",
 	long_description = " Intended for microtubules in neurites, but build to be much more flexible.",
-	version="0.0.2",
-	keywords = ["python", "pytorch", "stochast simulation", "SSA", "GPU", "Gillespie", "cytoskeleton", "spatial modeling"],
+	version="0.1.0",
+	keywords = ["python", "pytorch", "numba", "cuda", "stochastic simulation",
+				"SSA", "GPU", "Gillespie", "cytoskeleton", "spatial modeling"],
 	packages=find_packages()
       )
